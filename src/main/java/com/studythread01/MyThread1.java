710a64fb18d918c1ddab48b3626d01bb257a1520
Kevin <18322389273@163.com>
2018-04-20 20:50:08 +0800

    添加线程创建类

---
+++ src/main/java/com/studythread01/MyThread1.java
@@ -0,0 +1,32 @@
+package com.studythread01;
+
+import org.omg.CORBA.PUBLIC_MEMBER;
+
+public class MyThread1 extends Thread{
+
+	@Override
+	public void run() {
+		try {
+			Thread.sleep(10000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("run--" + Thread.currentThread().getName());
+	}
+	
+	
+	public static void main(String[] args) {
+		//1.第一步继承Thread类
+		//2.重写Thread类的run()方法
+		//3.创建一个线程实例
+		//4.调用实例方法 start()启动线程
+		
+		MyThread1 myThread1 = new MyThread1();
+		myThread1.start();
+		
+		System.out.println("main --" + Thread.currentThread().getName());
+		
+	}
+
+}
---
+++ src/main/java/com/studythread01/MyThread2.java
@@ -0,0 +1,10 @@
+package com.studythread01;
+
+public class MyThread2 extends Thread {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
